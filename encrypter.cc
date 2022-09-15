6f574ec0ed3321538347bf653ca5b0ec2fec9133
AnthonyDade <adade@DESKTOP-TVN41Q5.localdomain>
2022-09-14 23:06:33 -0400

    inital commit

---
+++ encrypter.cc
@@ -0,0 +1,20 @@
+/*
+
+    Anthony Dade
+    Ohio University
+    CS 4900 - Software Security
+    Assignment 2
+
+Simplified DES Encrypter that receives a string, a plaintext, on standard in and outputs the ciphertext on standard out. 
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void main(int argc, char* argv[]) 
+{ 
+    
+}
